e8c5a3a6c27e1bb837636806f4c68b86fb6ffc4a
jhoncarter7 <rainikhil044@gmail.com>
2024-05-02 01:34:31 +0530

    Add WeatherCard component to display weather data in App.jsx

--- weathervue/src/App.jsx
+++ weathervue/src/App.jsx
@@ -1,9 +1,14 @@
-
-
+import WeatherCard from "./components/WeatherCard"
+const res = await fetch(
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=RZ2MPVLFWPHPNG4Z2ARL7CGGH"
+);
 function App() {
   return (
-    <div>App</div>
+    <div className="bg-gray-700 h-screen">
+      <WeatherCard/>
+    </div>
   )
+
 }
 
 export default App
---
+++ weathervue/src/components/WeatherCard.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+function WeatherCard() {
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=RZ2MPVLFWPHPNG4Z2ARL7CGGH"
+      );
+      const data = await res.json();
+      console.log(data);
+      setWeatherData([data]);
+    };
+    fetchData();
+  }, []);
+  console.log(weatherData);
+  return (
+    <div className="mx-auto max-w-lg  rounded-xl shadow-lg overflow-hidden bg-neutral-900 text-white">
+      {weatherData?.map((W, indx) => (
+        <div key={indx} className="px-6 py-4 text-white">
+          <div className="flex justify-between">
+            <h2 className="text-xl font-bold ">{W.address}</h2>
+            <p className="">{W.currentConditions.datetime}</p>
+          </div>
+
+          <div className="flex items-center justify-center mt-4">
+            {/* <img
+              src={W.currentConditions.icon}
+              alt="Weather Icon"
+              className="w-12 h-12"
+            /> */}
+            <div className="flex flex-col gap-2 mb-6">
+              <h1 className="text-6xl font-bold  ml-4">
+                {(((W.currentConditions.temp - 32) * 5) / 9).toFixed(0)} °C
+              </h1>
+              <div className="flex gap-4">
+                <p>{W.currentConditions.conditions} </p>
+                <p>Feels like {W.currentConditions.feelslike}* F</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between">
+          <div >
+            <p className="mt-2">PRESSURE</p>
+            <p>{W.currentConditions.pressure}</p>
+          </div>
+          <div>
+            <p className="mt-2">WIND</p>
+            <p>{W.currentConditions.windspeed}mp/h</p>
+          </div>
+          <div>
+            <p className="mt-2">VISIBILITY</p>
+            <p>{W.currentConditions.visibility}</p>
+          </div>
+          <div>
+            <p className="mt-2">HUMIDITY</p>
+            <p>{W.currentConditions.humidity}</p>
+          </div>
+          </div>
+        </div>
+      ))}
+
+      {/* Additional details section - you can customize this */}
+      <div className=" px-6 py-4">
+        {/* Add more details like humidity, wind, etc. */}
+      </div>
+    </div>
+  );
+}
+
+export default WeatherCard;
